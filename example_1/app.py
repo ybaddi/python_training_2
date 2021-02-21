68352fbccc82d89b35b67323bed4b91b5b08af98
baddi <baddiyoussef85@gmail.com>
2021-02-21 15:02:31 +0100

    first example

---
+++ example_1/app.py
@@ -0,0 +1,104 @@
+
+# comment
+print("hello world")   # comment
+# comment
+# comment
+a=3
+print(a)
+
+# nom = input("Entrer votre nom ")
+nom='nom'
+print(nom)
+# age = int(input("Entrer votre age ")) # input me donne une chaine de caractere
+age=24
+print(age * 2)
+
+
+# int()
+print(age * 2)
+
+# 
+# poids = float(input("Entrer votre poids "))
+poids = 29.6
+print(poids)
+
+
+# affichage de plus d'un element
+# str = input("Entrer votre nom ")
+str  = 'str'
+print("Bonjour, ", str)
+print("Bonjour, {0}".format(str))
+print("Bonjour, {0}, j'ai {1} ans".format(str, age))
+
+#optimisation
+# >>> id(a)
+# 4493152400
+# >>> id(1)
+# 4493152400
+# >>> b=1
+# >>> b
+# 1
+# >>> id(b)
+# 4493152400
+# >>> id(a)
+# 4493152400
+# >>> 
+# >>> 
+# >>> x=[]
+# >>> y=[]
+# >>> id(x)
+# 4495713480
+# >>> id(y)
+# 4494774472
+# >>> 
+
+# variable en python
+# valeur alphanumerique
+# valeur numerique
+# valeur complexe
+
+print("hello worl")
+
+print(""" hello 
+           world """)
+
+accent = 'je vais être chez moi'
+accent.encode('ISO-8859-1')
+print(accent)
+
+int
+long
+bool
+float
+nombre complexe
+decimal #2listOfWords = ['this','is','a','list','of','words']
+sequence[i-1]
+
+squence[-1]    # last element
+sequence[-n]     # first element
+sequence[2:4]
+
+len() #
+min() #
+max() #
+sum() #
+
+string et les unicodes
+chr
+string => unicode with unicode()
+unicode => string str()
+
+
+%s: str()
+%d: decimal signe
+%o: octal non signe 
+%u: decimal non signe
+%x ou %X: hexadec
+%e ou %E  : float avec virgule flo
+%f reel
+
+
+>>> 1 in [1,2,3]            # O(n*n)
+True
+>>> 1 in set([1,2,3])       #(n) é
+True
